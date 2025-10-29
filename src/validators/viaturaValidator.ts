0ecb1d1fc56aebc6a81cf646c605cd61843ffc37
Manoel Olímpio <olimpiommelo@gmail.com>
2025-10-29 15:35:28 -0300

    Revert "Revert "alterando main""

--- src/validators/viaturaValidator.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const viaturaSchema = z.object({
-  body: z.object({
-    tipo_vt: z.string({ required_error: 'O tipo da viatura é obrigatório.' }),
-    numero_viatura: z.string({ required_error: 'O número da viatura é obrigatório.' }).min(1), 
-    id_unidade_operacional_fk: z.string({ required_error: 'O ID da unidade operacional é obrigatório.' }).uuid(),
-  }),
-});
