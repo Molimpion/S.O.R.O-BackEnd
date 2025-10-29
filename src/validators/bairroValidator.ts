33d5a4ce6c5230e7bb9895fd8da3c72e51663111
joaomatheus27 <Joaomth82@gmail.com>
2025-10-29 14:57:00 -0300

    aaaaaaaaaa

--- src/validators/bairroValidator.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const bairroSchema = z.object({
-  body: z.object({
-    nome_bairro: z.string({ required_error: 'O nome do bairro é obrigatório.' }).min(1), 
-    regiao: z.string().optional(),
-    ais: z.string().optional(),
-  }),
-});
--- src/validators/subgrupoValidator.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const subgrupoSchema = z.object({
-  body: z.object({
-    descricao_subgrupo: z.string({ required_error: 'A descrição do subgrupo é obrigatória.' }).min(3),
-    id_grupo_fk: z.string({ required_error: 'O ID do grupo é obrigatório.' }).uuid(),
-  }),
-});
