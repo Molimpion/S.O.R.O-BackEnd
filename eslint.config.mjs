5d3a3e005444052a468ffaf80a1b2d82e2309b5e
Manoel Olímpio <olimpiommelo@gmail.com>
2025-12-06 17:09:42 -0300

    feat: adicionando eslint e suas configurações

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,12 @@
-// eslint.config.mjs
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
   {
-    // Ignorar pastas de build e arquivos gerados
     ignores: ["dist/**", "node_modules/**", "coverage/**", "**/*.d.ts", "prisma/migrations/**"]
   },
   {
-    // Aplicar globais do Node (process, module, require, etc) para TODOS os arquivos JS/TS
     files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: { 
       globals: globals.node 
@@ -20,8 +17,7 @@ export default [
   {
     files: ["**/*.{ts,js}"],
     rules: {
-      // Regras personalizadas
-      "@typescript-eslint/no-explicit-any": "warn", // Apenas avisa, não quebra o build
+      "@typescript-eslint/no-explicit-any": "warn",
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn", 
